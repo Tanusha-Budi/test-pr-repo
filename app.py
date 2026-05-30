2338693c40b53dfc4fe6cdb79004fdc41fffd15e
Tanusha Budi <tanushabudi@Tanushas-MacBook-Air.local>
2026-05-30 08:49:04 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def divide(a, b):
+    return a / b  # bug: no check for division by zero
+
+def greet(name):
+    print("Hello " + name)  # should use f-string
+
+def get_user(users, id):
+    for i in range(len(users)):  # bad practice: should use enumerate
+        if users[i]["id"] == id:
+            return users[i]
+
+x = 10
+y = 0
+print(divide(x, y))  
